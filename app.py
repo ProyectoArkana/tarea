3725ccf19246ba4e29cabc193638cd804d6218af
LyenZo <marcosj.9807@gmail.com>
2026-02-04 14:08:11 -0600

    se hizo el flask db migrate

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask
 from controllers.HomeController import blueprint_home
+from extentions import db, migrate
+from config import Config 
+from models.User import User
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(blueprint_home,url_prefix='/api/')
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(blueprint_home, url_prefix='/api/')
+
     @app.route('/')
     def home():
         return {'msj': 'hola mundo'}
 
     return app
 
-
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=  True, 
-            host= '0.0.0.0')
+    app.run(
+        debug=True,
+        host='0.0.0.0'
+    )
---
+++ models/User.py
@@ -0,0 +1,7 @@
+from extentions import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(80))
